f535f05f9d0cfc530bd09294851798d07081c720
Joseph <joedt100@gmail.com>
2022-07-13 21:05:08 +0100

    added lib directory for the purposes of making an installer which pulls straight from github

---
+++ build.ps1
@@ -0,0 +1,2 @@
+mkdir build
+javac -d build -target 17 -cp "build;lib\lwjgl.jar;lib\lwjgl-glfw.jar;lib\lwjgl-glfw-natives-windows.jar;lib\lwjgl-natives-windows.jar;lib\lwjgl-openal.jar;lib\lwjgl-openal-natives-windows.jar;lib\lwjgl-opencl.jar;lib\lwjgl-opengl.jar;lib\lwjgl-opengl-natives-windows.jar;lib\lwjgl-stb.jar;lib\lwjgl-stb-natives-windows.jar" src/*.java src/game/*.java src/game/engine/*.java  src/game/engine/rendering/*.java src/game/engine/rendering/math/*.java
---
+++ run.ps1
@@ -0,0 +1,1 @@
+java -classpath "build;lib\lwjgl.jar;lib\lwjgl-glfw.jar;lib\lwjgl-glfw-natives-windows.jar;lib\lwjgl-natives-windows.jar;lib\lwjgl-openal.jar;lib\lwjgl-openal-natives-windows.jar;lib\lwjgl-opencl.jar;lib\lwjgl-opengl.jar;lib\lwjgl-opengl-natives-windows.jar;lib\lwjgl-stb.jar;lib\lwjgl-stb-natives-windows.jar" Main 1280 720 false 1
